c4796b80f49ec1e950b45effecd8867717cf82ef
dinuka555 <75948541+dinuka555@users.noreply.github.com>
2021-01-28 16:47:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello from Python!"
-  
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return "Hello Nuclear Geeks"
+
 if __name__ == '__main__':
-    app.run("0.0.0.0", port=5000, debug=True)
+    app.run()
