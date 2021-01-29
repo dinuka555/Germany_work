d47740f7793d09539d9d2b8d820b207a3dd2ab08
dinuka555 <75948541+dinuka555@users.noreply.github.com>
2021-01-29 12:25:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = '10.4.13.36'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ' '
-app.config['MYSQL_DB'] = 'satdata'
+app.config['MYSQL_DB'] = 'test'
 
 mysql = MySQL(app)
 
@@ -15,15 +15,15 @@ mysql = MySQL(app)
 def index():
     if request.method == "POST":
         details = request.form
-        location = details['UK']
-        orbit = details['59.7']
+        firstName = details['fname']
+        lastName = details['lname']
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO satdata(location, orbit) VALUES (mas, kos)", (location, orbit))
+        #cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
+        cur.execute("INSERT INTO messages(message) VALUES (%s)",(firstName))
         mysql.connection.commit()
         cur.close()
         return 'success'
     return render_template('web1.html')
-    #return "Hello Nuclear Geeks"
 
 if __name__ == '__main__':
     app.run("0.0.0.0", port=5000, debug=True)
