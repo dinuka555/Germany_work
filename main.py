cb3cfdf7901a06af172f2a0fc435c5b0a7514bc4
dinuka555 <75948541+dinuka555@users.noreply.github.com>
2021-01-29 13:02:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ def index():
         #lastName = details['lname']
         cur = mysql.connection.cursor()
         #cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
-        cur.execute("INSERT INTO messages(message) VALUES (%s)",(firstName))
+        #sql = "INSERT INTO image VALUES (%s)"
+
+        cur.execute("INSERT INTO messages(message) VALUES ('%s')",(firstName))
         mysql.connection.commit()
         cur.close()
         return 'success'
