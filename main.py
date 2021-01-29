874ed25bb3289b3362b6a7135833bffdd3fdf2fd
dinuka555 <75948541+dinuka555@users.noreply.github.com>
2021-01-29 12:48:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = '10.4.13.36'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ' '
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'test'
 
 mysql = MySQL(app)
