f840f71362ad9e0cfaeaa7367616f26cbdc03197
dinuka555 <75948541+dinuka555@users.noreply.github.com>
2021-01-29 16:41:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,5 +36,31 @@ def index():
         return 'success'
     return render_template('web1.html')
 
+
+@app.route('/', methods=['GET', 'POST'])
+def index1():
+    if request.method == "POST":
+        details = request.form
+        log_time = details['log']
+        _ID = details['id']
+        orbit = details['or']
+        sendfrom = details['send']
+        location = details['loc']
+        
+        cur = mysql.connection.cursor()
+        #cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
+        #sql = "INSERT INTO image VALUES (%s)"
+
+        #cur.execute("INSERT INTO messages(message) VALUES ('%s')",(firstName))
+        #cur.execute("INSERT INTO messages(message) VALUES ('testdata1')")
+        #    cur.execute("INSERT INTO song (title) VALUES ('%s')" %(i,))
+        cur.execute("INSERT INTO satdata(messages) VALUES ('%s','%s','%s','%s','%s')" %(log_time,_ID,orbit,sendfrom,location))
+
+
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
+    return render_template('web1.html')
+
 if __name__ == '__main__':
     app.run("0.0.0.0", port=5000, debug=True)
