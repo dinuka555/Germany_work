4413ffbb2459490bb19f179e2d6babe709a2d164
dinuka555 <75948541+dinuka555@users.noreply.github.com>
2021-01-29 14:18:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def index():
         #cur.execute("INSERT INTO messages(message) VALUES ('%s')",(firstName))
         #cur.execute("INSERT INTO messages(message) VALUES ('testdata1')")
         #    cur.execute("INSERT INTO song (title) VALUES ('%s')" %(i,))
-        cur.execute("INSERT INTO messages(message) VALUES ('%s')" %(firstName,))
+        cur.execute("INSERT INTO messages(message) VALUES ("%s")" %(firstName,))
 
 
         mysql.connection.commit()
